84c26e2f65265df02c46c5f60eba50b9aebfefc2
leosaline <leosaline@gmail.com>
2020-05-07 19:16:03 -0300

    refactor

--- src/main/java/com/saline/naton/entity/Purchase.java
+++ src/main/java/com/saline/naton/entity/Purchase.java
@@ -3,6 +3,8 @@ package com.saline.naton.entity;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,6 +13,7 @@ import javax.persistence.Table;
 public class Purchase {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Date datePurchase;
 	private Long user;
